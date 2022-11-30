c59631d552de908670e7ecbe90689b62d20abae8
Darwin G <dgarcia26@cuc.edu.co>
2022-11-29 19:07:13 -0500

    final-2

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Footer from "./Footer";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 const Inicio = () =>{
